105032500a76701b39b680a5ac6d6f1f043cc51a
Nunes <GilsonNunes_dev@outlook.com>
2021-02-11 21:16:09 -0300

    adicionado lógica para jogar sozinho

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@ for (let i = 0; i < boxes.length; i++) {
 
       if (player1 == player2) {
         player1++;
+
+        if (secondPlayer == "ai-player") {
+          computerPlay();
+          player2++;
+        }
       } else {
         player2++;
       }
@@ -230,5 +235,29 @@ function declareWinner(winner) {
 
   for (let i = 0; i < boxesToRemove.length; i++) {
     boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+  }
+}
+
+function computerPlay() {
+  let cloneO = o.cloneNode(true);
+  let counter = 0;
+  let filled = 0;
+
+  for (let i = 0; i < boxes.length; i++) {
+    let randomNumber = Math.floor(Math.random() * 5);
+
+    if (boxes[i].childNodes[0] == undefined) {
+      if (randomNumber <= 1) {
+        boxes[i].appendChild(cloneO);
+        counter++;
+        break;
+      }
+    } else {
+      filled++;
+    }
+  }
+
+  if (counter == 0 && filled < 9) {
+    computerPlay();
   }
 }
